1818ebe4ddd45f1796709eae92366261cd1e72e8
Людмила Бубенко <Tyuiu.bubenkolg@outlook.com>
2025-11-27 00:22:55 +0500

    Task2

---
+++ Tyuiu.BubenkoLG.Sprint5.Task2.V7.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BubenkoLG.Sprint5.Task2.V7.Lib;
+using System.IO;
+
+namespace Tyuiu.BubenkoLG.Sprint5.Task2.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\Людмила Георгиевна\Desktop\source\repos\Tyuiu.BubenkoLG.Sprint5\Tyuiu.BubenkoLG.Sprint5.Task2.V7\bin\Debug\net8.0\OutPutFileTask2.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.BubenkoLG.Sprint5.Task2.V7/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Tyuiu.BubenkoLG.Sprint5.Task2.V7.Lib;
+
+namespace Tyuiu.BubenkoLG.Sprint5.Task2.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Бубенко Л. Г. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                ");
+            Console.WriteLine("* Тема: Класс File. Запись набора данных в текстовый файл.                 ");
+            Console.WriteLine("* Задание #2                                                               ");
+            Console.WriteLine("* Вариант #7                                                               ");
+            Console.WriteLine("* Выполнила: Бубенко Людмила Георгиевна | СМАРТб-25-1                      ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 ");
+            Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3, заполненный значениями        ");
+            Console.WriteLine("* с клавиатуры. Заменить нечётные элементы массива на 0. Результат         ");
+            Console.WriteLine("* сохранить в файл outPutFileTask2.csv и вывести на консоль.               ");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+            Console.WriteLine("***************************************************************************");
+            Console.Write("Введите длину строк массива: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите длину столбцов массива: ");
+            int columns = Convert.ToInt32(Console.ReadLine());
+            int[,] mtrx = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"Введите {i}{j} элемент матрицы: ");
+                    mtrx[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Массив:");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               ");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(mtrx);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан");
+
+            Console.ReadKey();
+        }
+    }
+}
