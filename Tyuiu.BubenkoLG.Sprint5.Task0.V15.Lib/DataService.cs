b6a9c756dcaadbba538047838b1942f8a15f6fd1
Людмила Бубенко <Tyuiu.bubenkolg@outlook.com>
2025-11-27 00:05:21 +0500

    Add project files.

---
+++ Tyuiu.BubenkoLG.Sprint5.Task0.V15.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.BubenkoLG.Sprint5.Task0.V15.Lib
+{
+    public class DataService : ISprint5Task0V15
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Path.GetTempPath()}\OutPutFileTask0.txt";
+            double y = x / (Math.Pow(x, 3) + 2.0);
+            y = Math.Round(y, 3);
+            File.WriteAllText(path, Convert.ToString(y));
+            return path;
+        }
+    }
+}
