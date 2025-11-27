347e310fa90efa43035c816ef6273f691d9c0aab
Людмила Бубенко <Tyuiu.bubenkolg@outlook.com>
2025-11-27 16:17:34 +0500

    Task4

---
+++ Tyuiu.BubenkoLG.Sprint5.Task4.V12.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BubenkoLG.Sprint5.Task4.V12.Lib;
+using System.IO;
+
+namespace Tyuiu.BubenkoLG.Sprint5.Task4.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\Людмила Георгиевна\Desktop\source\repos\Tyuiu.BubenkoLG.Sprint5\Tyuiu.BubenkoLG.Sprint5.Task4.V12\bin\Debug\net8.0\InPutDataFileTask4V12.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.BubenkoLG.Sprint5.Task4.V12/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Tyuiu.BubenkoLG.Sprint5.Task4.V12.Lib;
+
+namespace Tyuiu.BubenkoLG.Sprint5.Task4.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Бубенко Л. Г. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                ");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла.                                 ");
+            Console.WriteLine("* Задание #4                                                               ");
+            Console.WriteLine("* Вариант #12                                                              ");
+            Console.WriteLine("* Выполнила: Бубенко Людмила Георгиевна | СМАРТб-25-1                      ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 ");
+            Console.WriteLine("* Дан файл С:\\DataSprint5\\InPutDataFileTask4V0.txt (файл взять из архива ");
+            Console.WriteLine("* согласно вашему варианту. Создать папку в ручную С:\\DataSprint5\\ и     ");
+            Console.WriteLine("* скопировать в неё файл) в котором есть вещественное значение. Прочитать  ");
+            Console.WriteLine("* значение из файла и подставить вместо Х в формуле                        ");
+            Console.WriteLine("*          3     x                                                         ");
+            Console.WriteLine("* y = cos(x ) + ---                                                        ");
+            Console.WriteLine("*                2                                                         ");
+            Console.WriteLine("* Вычислить значение по формуле (Полученное значение округлить до трёх     ");
+            Console.WriteLine("* знаков после запятой) и вернуть полученный результат на консоль.         ");
+            //string path = @"C:\Users\Людмила Георгиевна\Desktop\source\repos\Tyuiu.BubenkoLG.Sprint5\Tyuiu.BubenkoLG.Sprint5.Task4.V12\bin\Debug\net8.0\InPutDataFileTask4V12.txt";
+
+            string path = $@"{Path.GetTempPath()}\InPutDataFileTask4V12.txt";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         ");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               ");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
